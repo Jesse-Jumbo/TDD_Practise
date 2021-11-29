3814bbbc07420bc93b706c32a07cbfce3fc96f30
Jesse <jessie97013@gmail.com>
2021-11-29 17:04:08 +0800

    gitignore forty love success

--- first_tennis.py
+++ first_tennis.py
@@ -1,9 +1,7 @@
 class Tennis():
     def __init__(self):
-        self.first_player_score = 0
-
-    def lookup_score(self):
-        lookup_score = {
+        self.first_player_score_times = 0
+        self.lookup_score = {
             0: "love",
             1: "fifteen",
             2: "thirty",
@@ -11,13 +9,11 @@ class Tennis():
         }
 
     def score(self):
-        if self.first_player_score == 1:
-            return f"{self.lookup_score()[self.first_player_score]} love"
+        if self.first_player_score_times > 0:
+            return f"{self.lookup_score[self.first_player_score_times]} love"
         return "love all"
 
-    def first_player_score(self, times):
-        self.given_first_player_score_times(times)
+    def first_player_score(self):
+        self.first_player_score_times += 1
 
-    def given_first_player_score_times(self, times):
-        for i in range(times):
-            self.first_player_score += 1
+
--- test_first_tennis.py
+++ test_first_tennis.py
@@ -2,6 +2,7 @@ import unittest
 
 from first_tennis import *
 
+
 class TennisTest(unittest.TestCase):
     def setUp(self):
         self.tennis = Tennis()
@@ -10,16 +11,20 @@ class TennisTest(unittest.TestCase):
         self.score_should_be("love all")
 
     def test_fifteen_love(self):
-        self.tennis.first_player_score(1)
+        self.given_first_player_score_times(1)
         self.score_should_be("fifteen love")
 
     def test_thirty_love(self):
-        self.tennis.first_player_score(2)
+        self.given_first_player_score_times(2)
         self.score_should_be("thirty love")
 
     def test_forty_love(self):
-        self.tennis.first_player_score(3)
+        self.given_first_player_score_times(3)
         self.score_should_be("forty love")
+
+    def given_first_player_score_times(self, times):
+        for i in range(times):
+            self.tennis.first_player_score()
 
     def score_should_be(self, expected):
         self.assertEqual(self.tennis.score(), expected)
