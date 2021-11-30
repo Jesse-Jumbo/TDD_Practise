0d26eb07e3f5dcfdf77e118df573f918017c966d
Jesse <jessie97013@gmail.com>
2021-11-30 12:14:30 +0800

    deuce success in test second tennis

--- second_tennis.py
+++ second_tennis.py
@@ -10,10 +10,13 @@ class Tennis:
         }
 
     def score(self):
-        if self.second_player_score != 0:
-            return f"love {self.lookup_score[self.second_player_score]}"
-        if self.first_player_score != 0:
-            return f"{self.lookup_score[self.first_player_score]} love"
+        if self.second_player_score != 0 or self.first_player_score != 0:
+            if self.second_player_score < 3 and self.first_player_score == 0:
+                return f"love {self.lookup_score[self.second_player_score]}"
+            if self.first_player_score < 3 and self.second_player_score == 0:
+                return f"{self.lookup_score[self.first_player_score]} love"
+            if self.second_player_score >= 3:
+                return "deuce"
         return "love all"
 
     def given_first_player_score(self):
