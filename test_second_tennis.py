469fa2036a71cec01fb96c3553b3537ff07048ea
Jesse <jessie97013@gmail.com>
2021-11-30 11:33:30 +0800

    add fifteen love in test second tennis

--- test_second_tennis.py
+++ test_second_tennis.py
@@ -4,9 +4,18 @@ from second_tennis import Tennis
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def setUp(self):
         self.tennis = Tennis()
-        self.assertEqual(self.tennis.score(), "love all")  # add assertion here
+
+    def test_fifteen_love(self):
+        self.given_first_player_score()
+        self.score_should_be("fifteen love")
+
+    def test_love_all(self):
+        self.score_should_be("love all")
+
+    def score_should_be(self, expected):
+        self.assertEqual(self.tennis.score(), expected)  # add assertion here
 
 
 if __name__ == '__main__':
