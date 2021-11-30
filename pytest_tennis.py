754745386e3ee0b2015a0a14366b0931f8bbffb7
Jesse <jessie97013@gmail.com>
2021-11-30 18:36:03 +0800

    love fifteen success

--- pytest_tennis.py
+++ pytest_tennis.py
@@ -1,5 +1,6 @@
 class Tennis:
     def __init__(self):
+        self.second_player_score_times = 0
         self.first_player_score_times = 0
         self.lookup_score = {
             0: "love",
@@ -11,8 +12,13 @@ class Tennis:
     def score(self):
         if self.first_player_score_times != 0:
             return f"{self.lookup_score[self.first_player_score_times]} love"
-        else:
-            return "love all"
+        if self.second_player_score_times != 0:
+            return f"love {self.lookup_score[self.first_player_score_times]}"
+        return "love all"
+
 
     def given_first_player_score(self):
         self.first_player_score_times += 1
+
+    def given_second_player_score(self):
+        self.second_player_score_times += 1
--- test_pytest_tennis.py
+++ test_pytest_tennis.py
@@ -6,6 +6,11 @@ class TestTennisGame(object):
     def setup_class(self):
         self.tennis = Tennis()
 
+
+    def test_love_fifteen(self):
+        self.given_second_player_score_times(1)
+        assert self.tennis.score() == "love fifteen"
+
     def test_thirty_love(self):
         self.given_first_player_score_times(2)
         assert self.tennis.score() == "thirty love"
@@ -20,3 +25,8 @@ class TestTennisGame(object):
     def given_first_player_score_times(self, times):
         for i in range(times):
             self.tennis.given_first_player_score()
+
+    def given_second_player_score_times(self, times):
+        for i in range(times):
+            self.tennis.given_second_player_score()
+
