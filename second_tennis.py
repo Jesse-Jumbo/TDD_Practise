5c709ee5eba4fbf79de53f7dcced3f89e8440e7c
Jesse <jessie97013@gmail.com>
2021-11-30 11:46:14 +0800

    fifteen love success in test second tennis

--- second_tennis.py
+++ second_tennis.py
@@ -1,3 +1,11 @@
 class Tennis:
+    def __init__(self):
+        self.first_player_score = 0
+
     def score(self):
+        if self.first_player_score != 0:
+            return "fifteen love"
         return "love all"
+
+    def given_first_player_score(self):
+        self.first_player_score += 1
--- test_second_tennis.py
+++ test_second_tennis.py
@@ -8,11 +8,15 @@ class MyTestCase(unittest.TestCase):
         self.tennis = Tennis()
 
     def test_fifteen_love(self):
-        self.given_first_player_score()
+        self.given_first_player_score_times(1)
         self.score_should_be("fifteen love")
 
     def test_love_all(self):
         self.score_should_be("love all")
+
+    def given_first_player_score_times(self, times):
+        for i in range(times):
+            self.tennis.given_first_player_score()
 
     def score_should_be(self, expected):
         self.assertEqual(self.tennis.score(), expected)  # add assertion here
